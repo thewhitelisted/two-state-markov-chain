646729b1277b3c8a067aaeb2b94194b042025aa5
thewhitelisted <clee2k6@outlook.com>
2025-03-09 13:55:40 -0400

    added: comments

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,27 @@
+// two state markov chain simulation
+// 1. white -> white or white -> black
+// 2. black -> white or black -> black
+
+// imports
 use rand::thread_rng;
 use rand::seq::SliceRandom;
 
+// draws a ball randomly from a jar
 fn draw_ball(jar: &mut Vec<char>) -> char {
     jar.shuffle(&mut thread_rng());
     return jar[0];
 }
 
+// creates a sequence that follows our rules
 fn derive_sequence(jar_w: &mut Vec<char>, jar_b: &mut Vec<char>, num_draws: usize) -> Vec<char> {
     let mut sequence: Vec<char> = Vec::new();
-    // draws start from W
+
+    // draws start from W, can start from either W or B
     sequence.push('W');
     let mut a = draw_ball(jar_w);
     sequence.push(a);
 
+    // drawing sequence
     for _ in 1..num_draws {
         if a == 'W' {
             a = draw_ball(jar_w);
@@ -25,6 +34,7 @@ fn derive_sequence(jar_w: &mut Vec<char>, jar_b: &mut Vec<char>, num_draws: usiz
     return sequence;
 }
 
+// checks if a jump from a jar has occured, returns the jump state and the colour of the jar
 fn is_jump(prev: char, curr: char) -> Option<(bool, char)> {
     if prev == 'W' && curr == 'B' {
         return Some((true, 'W'));
@@ -34,6 +44,7 @@ fn is_jump(prev: char, curr: char) -> Option<(bool, char)> {
     return None;
 }
 
+// calculates the transition probability matrix from a sequence
 fn seq_to_transition_probability(seq: Vec<char>) -> Vec<Vec<f64>> {
     let mut w_wb = 0.0;
     let mut draws_from_white = 0.0;
@@ -41,8 +52,10 @@ fn seq_to_transition_probability(seq: Vec<char>) -> Vec<Vec<f64>> {
     let mut draws_from_black = 0.0;
 
     let mut result: Vec<Vec<f64>>= Vec::new();
+    // start at the second element because the first one is predetermined.
     let mut prev: char = seq[1];
 
+    // iterate and count the number of jumps and draws from each jar
     for i in 2..seq.len() {
         let curr: char = seq[i];
 
@@ -63,6 +76,7 @@ fn seq_to_transition_probability(seq: Vec<char>) -> Vec<Vec<f64>> {
         prev = curr;
     }
 
+    // calculate the transition probabilities
     let wb: f64 = w_wb / draws_from_white as f64;
     let bw: f64 = b_bw / draws_from_black as f64;
     let ww: f64 = 1.0 - wb;
