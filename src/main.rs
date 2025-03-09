e517b440cfc50cb8a23590e7eb89d0041f72c935
thewhitelisted <clee2k6@outlook.com>
2025-03-09 15:49:06 -0400

    impl: steady state and prediction using stochastic matrix

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,58 @@
 use rand::thread_rng;
 use rand::seq::SliceRandom;
 
+// computes the steady state of a markov chain
+// shows long run behaviour of the chain
+fn steady_state(transition_matrix: &Vec<Vec<f64>>) -> Vec<f64> {
+    let alpha: f64 = transition_matrix[0][0];
+    let beta: f64 = transition_matrix[1][1];
+
+    let x: f64 = (1.0 - beta) / (2.0 - (alpha + beta));
+    let y: f64 = (1.0 - alpha) / (2.0 - (alpha + beta));
+
+    return vec![x, y];
+}
+
+// calculates the dot product of two vectors
+fn dot_product(v1: &Vec<f64>, v2: &Vec<f64>) -> f64 {
+    let mut result = 0.0;
+    for i in 0..v1.len() {
+        result += v1[i] * v2[i];
+    }
+    return result;
+}
+
+// predicts the next state given the current state for a number of draws. if it reaches steady state it will return 'W'
+fn predict_next_state(transition_matrix: &Vec<Vec<f64>>, current_state: char, num_draws: usize) -> Vec<char> {
+    // initial state vector
+    let mut v0: Vec<f64> = vec![0.0, 0.0];
+    if current_state == 'W' {
+        v0[0] = 1.0;
+    } else {
+        v0[1] = 1.0;
+    }
+
+    let mut futures: Vec<Vec<f64>> = vec![vec![0.0, 0.0]; num_draws];
+    futures[0] = v0;
+    let mut results: Vec<char> = Vec::new();
+
+    for i in 1..num_draws {
+        futures[i][0] = dot_product(&futures[i-1], &transition_matrix[0]);
+        futures[i][1] = dot_product(&futures[i-1], &transition_matrix[1]);
+    }
+
+
+    for i in 0..num_draws {
+        if futures[i][0] < futures[i][1] {
+            results.push('B');
+        } else {
+            results.push('W');
+        }
+    }
+
+    return results;
+}
+
 // draws a ball randomly from a jar
 fn draw_ball(jar: &mut Vec<char>) -> char {
     jar.shuffle(&mut thread_rng());
@@ -92,8 +144,9 @@ fn main() {
     // proportion of white and black balls in the jars
     let mut jar_w: Vec<char> = "WWBBBBBBBB".chars().collect();
     let mut jar_b: Vec<char> = "WWWWWWBBBB".chars().collect();
-    let seq = derive_sequence(&mut jar_w, &mut jar_b, 100);
-    let transition_matrix = seq_to_transition_probability(seq.clone());
+    let seq: Vec<char> = derive_sequence(&mut jar_w, &mut jar_b, 100);
+    let transition_matrix: Vec<Vec<f64>> = seq_to_transition_probability(seq.clone());
+    let actual_matrix: Vec<Vec<f64>> = vec![vec![0.17, 0.83], vec![0.56, 0.44]];
 
     // print transition matrix with 2 decimal places
     println!("Transition Matrix:");
@@ -101,4 +154,12 @@ fn main() {
         println!("{:.2} | {:.2}", row[0], row[1]);
     }
     println!("Sequence: {:?}", seq);
+
+    // prediction, reaches steady state after 40 draws
+    let predictions: Vec<char> = predict_next_state(&actual_matrix, 'W', 41);
+    println!("Predictions: {:?}", predictions);
+
+    // steady state
+    let steady: Vec<f64> = steady_state(&actual_matrix);
+    println!("Steady State: {:?}", steady);
 }
